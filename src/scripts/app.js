8691fd2598790cfce9f9b43d035e9efe3700b885
Momin Naseem <mominn3935@gmail.com>
2023-01-21 18:43:05 -0500

    Comments and formatting

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,297 +1,354 @@
-import THREE from './three.r110.js';
-import TweenMax from './tweenmax.1.20.3.js';
-import OBJLoader from './three.r110.objloader.js';
-import OrbitControls from './three.r110.orbitcontrols.js';
-import 'styles/index.css';
+import THREE from "./three.r110.js";
+import TweenMax from "./tweenmax.1.20.3.js";
+import OBJLoader from "./three.r110.objloader.js";
+import OrbitControls from "./three.r110.orbitcontrols.js";
+import "styles/index.css";
 
 THREE = window.THREE;
 THREE.OrbitControls = OrbitControls;
-THREE.OBJLoader  = OBJLoader;
-
+THREE.OBJLoader = OBJLoader;
 
 export default class App {
-  init() {
-    this.group = new THREE.Object3D();
-    this.bgColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
-    this.gridSize = 40;
-	// List of buildings in the scene
-    this.buildings = [];
-    // this.fogConfig = {
-    // 	color: '#36454F',
-	// 	//color: '#353c3c',
-    // 	near: 5,
-    // 	far: 220
-    // };
+	init() {
+		this.group = new THREE.Object3D();
+		this.bgColor = window
+			.getComputedStyle(document.body, null)
+			.getPropertyValue("background-color");
+		this.gridSize = 40;
+		// List of buildings in the scene
+		this.buildings = [];
+		// this.fogConfig = {
+		// 	color: '#36454F',
+		// 	//color: '#353c3c',
+		// 	near: 5,
+		// 	far: 220
+		// };
 
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
 
-    this.createScene();
-    this.createCamera();
-    this.addCameraControls();
-    this.addFloor();
-    this.addBackgroundShape();
-    this.loadModels('https://raw.githubusercontent.com/ca-john/ca-john.github.io/main/homepage_buildings.obj', this.onLoadModelsComplete.bind(this));
-    this.animate();
+		this.createScene();
+		this.createCamera();
+		this.addCameraControls();
+		this.addFloor();
+		this.addBackgroundShape();
+		// Load models from private repository
+		this.loadModels(
+			"https://raw.githubusercontent.com/ca-john/ca-john.github.io/main/homepage_buildings.obj",
+			this.onLoadModelsComplete.bind(this)
+		);
+		this.animate();
 
-    this.pointLightObj3 = {
-      color: '#7393B3',
-      intensity: 10,
-      position: {
-        x: 80,
-        y: 110,
-        z: -80,
-      }
-    };
+		this.pointLightObj3 = {
+			color: "#7393B3",
+			intensity: 10,
+			position: {
+				x: 80,
+				y: 110,
+				z: -80,
+			},
+		};
 
-    this.addPointLight(this.pointLightObj3);
+		this.addPointLight(this.pointLightObj3);
+	}
 
-	
-	
-  }
+	createScene() {
 
-  createScene() {
-    this.scene = new THREE.Scene();
+		this.scene = new THREE.Scene();
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+		this.renderer.setSize(this.width, this.height);
 
-    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    this.renderer.setSize(this.width, this.height);
+		this.renderer.shadowMap.enabled = true;
+		//this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		// Variance Shadow Map for the building models, *might impact performance*
+		this.renderer.shadowMap.type = THREE.VSMShadowMap;
 
-    this.renderer.shadowMap.enabled = true;
-    //this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-	this.renderer.shadowMap.type = THREE.VSMShadowMap;
+		document.body
+			.querySelector(".canvas-wrapper")
+			.appendChild(this.renderer.domElement);
 
-    document.body.querySelector('.canvas-wrapper').appendChild(this.renderer.domElement);
+		//this.scene.fog = new THREE.Fog(this.fogConfig.color, this.fogConfig.near, this.fogConfig.far);
+		// Exponential fog that increases exponentially as the distance from the camera increases
+		// might be a render bottleneck
+		this.scene.fog = new THREE.FogExp2("#36454F", 0.01);
+	}
 
-    //this.scene.fog = new THREE.Fog(this.fogConfig.color, this.fogConfig.near, this.fogConfig.far);
-	this.scene.fog = new THREE.FogExp2('#36454F', 0.01);
+	createCamera() {
+		this.camera = new THREE.PerspectiveCamera(
+			20,
+			this.width / this.height,
+			1,
+			1000
+		);
+		this.camera.position.set(3, 50, 155);
 
-  }
+		this.scene.add(this.camera);
+	}
 
-  createCamera() {
-    this.camera = new THREE.PerspectiveCamera(20, this.width / this.height, 1, 1000);
-    this.camera.position.set(3, 50, 155);
+	addCameraControls() {
+		this.controls = new THREE.OrbitControls(
+			this.camera,
+			this.renderer.domElement
+		);
 
-    this.scene.add(this.camera);
-  }
+		this.controls.enabled = false;
+	}
 
-  addCameraControls() {
-    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+	addSpotLight() {
+		const light = { color: "#ff0000", x: 641, y: -462, z: 509 };
+		const spotLight = new THREE.SpotLight(light.color, 1);
 
-    this.controls.enabled = false;
-  }
+		spotLight.position.set(light.x, light.y, light.z);
+		spotLight.castShadow = true;
 
-  addSpotLight() {
-    const light = { color: '#ff0000', x: 641, y: -462, z: 509 };
-    const spotLight = new THREE.SpotLight(light.color, 1);
+		this.scene.add(spotLight);
+	}
 
-    spotLight.position.set(light.x, light.y, light.z);
-    spotLight.castShadow = true;
+	addAmbientLight() {
+		const light = { color: "#a00a0a" };
+		const ambientLight = new THREE.AmbientLight(light.color);
 
-    this.scene.add(spotLight);
-  }
+		this.scene.add(ambientLight);
+	}
 
-  addAmbientLight() {
-    const light = { color: '#a00a0a' };
-    const ambientLight = new THREE.AmbientLight(light.color);
+	addBackgroundShape() {
+		const planeGeometry = new THREE.PlaneGeometry(400, 100);
+		const planeMaterial = new THREE.MeshPhysicalMaterial({ color: "#fff" });
+		this.backgroundShape = new THREE.Mesh(planeGeometry, planeMaterial);
 
-    this.scene.add(ambientLight);
-  }
+		this.backgroundShape.position.y = 10;
+		this.backgroundShape.position.z = -150;
 
-  addBackgroundShape() {
-    const planeGeometry = new THREE.PlaneGeometry(400, 100);
-    const planeMaterial = new THREE.MeshPhysicalMaterial({ color: '#fff' });
-    this.backgroundShape = new THREE.Mesh(planeGeometry, planeMaterial);
+		this.scene.add(this.backgroundShape);
 
-    this.backgroundShape.position.y = 10;
-    this.backgroundShape.position.z = -150;
+		this.mouseX = 3;
+		this.lastMouseX = 3;
+		this.lastMouseY = 65;
+		this.lastScale = 155;
+		this.tiltFx = {
+			body: document.body,
+			docEl: document.documentElement,
+			getMousePos: (e, docScrolls) => {
+				let posx = 0;
+				let posy = 0;
+				if (!e) {
+					e = window.event;
+				}
+				if (e.pageX || e.pageY) {
+					posx = e.pageX;
+					posy = e.pageY;
+				} else if (e.clientX || e.clientY) {
+					posx = e.clientX + docScrolls.left;
+					posy = e.clientY + docScrolls.top;
+				}
+				return { x: posx, y: posy };
+			},
+			lerp: (a, b, n) => (1 - n) * a + n * b,
+			lineEq: (y2, y1, x2, x1, currentVal) => {
+				let m = (y2 - y1) / (x2 - x1);
+				let b = y1 - m * x1;
+				return m * currentVal + b;
+			},
+		};
 
-    this.scene.add(this.backgroundShape);
+		this.docheight = Math.max(
+			this.tiltFx.body.scrollHeight,
+			this.tiltFx.body.offsetHeight,
+			this.tiltFx.docEl.clientHeight,
+			this.tiltFx.docEl.scrollHeight,
+			this.tiltFx.docEl.offsetHeight
+		);
 
-    this.mouseX = 3;
-    this.lastMouseX = 3;
-    this.lastMouseY = 65;
-    this.lastScale = 155;
-    this.tiltFx = {
-      body: document.body,
-      docEl: document.documentElement,
-      getMousePos: (e, docScrolls) => {
-        let posx = 0;
-        let posy = 0;
-        if (!e) { e = window.event; }
-        if (e.pageX || e.pageY) {
-          posx = e.pageX;
-          posy = e.pageY;
-        }
-        else if (e.clientX || e.clientY) {
-          posx = e.clientX + docScrolls.left;
-          posy = e.clientY + docScrolls.top;
-        }
-        return { x: posx, y: posy }
-      },
-      lerp: (a, b, n) => (1 - n) * a + n * b,
-      lineEq: (y2, y1, x2, x1, currentVal) => {
-        let m = (y2 - y1) / (x2 - x1);
-        let b = y1 - m * x1;
-        return m * currentVal + b;
-      }
-    };
+		this.requestId = requestAnimationFrame(() => this.tilt());
 
-    this.docheight = Math.max(this.tiltFx.body.scrollHeight, this.tiltFx.body.offsetHeight, this.tiltFx.docEl.clientHeight, this.tiltFx.docEl.scrollHeight, this.tiltFx.docEl.offsetHeight);
+		window.addEventListener("mousemove", (ev) => {
+			const docScrolls = {
+				left: this.tiltFx.body.scrollLeft + this.tiltFx.docEl.scrollLeft,
+				top: this.tiltFx.body.scrollTop + this.tiltFx.docEl.scrollTop,
+			};
+			const mp = this.tiltFx.getMousePos(ev, docScrolls);
+			this.mouseX = mp.x - docScrolls.left;
+		});
 
-    this.requestId = requestAnimationFrame(() => this.tilt());
+		window.addEventListener(
+			"resize",
+			() =>
+				(this.docheight = Math.max(
+					this.tiltFx.body.scrollHeight,
+					this.tiltFx.body.offsetHeight,
+					this.tiltFx.docEl.clientHeight,
+					this.tiltFx.docEl.scrollHeight,
+					this.tiltFx.docEl.offsetHeight
+				))
+		);
 
-    window.addEventListener('mousemove', (ev) => {
-      const docScrolls = { left: this.tiltFx.body.scrollLeft + this.tiltFx.docEl.scrollLeft, top: this.tiltFx.body.scrollTop + this.tiltFx.docEl.scrollTop };
-      const mp = this.tiltFx.getMousePos(ev, docScrolls);
-      this.mouseX = mp.x - docScrolls.left;
-    });
+		window.onbeforeunload = () => {
+			window.cancelAnimationFrame(this.requestId);
+			window.scrollTo(0, 0);
+		};
+	}
 
-    window.addEventListener('resize', () => this.docheight = Math.max(this.tiltFx.body.scrollHeight, this.tiltFx.body.offsetHeight, this.tiltFx.docEl.clientHeight,
-      this.tiltFx.docEl.scrollHeight, this.tiltFx.docEl.offsetHeight));
+	tilt() {
+		this.lastMouseX = this.tiltFx.lerp(
+			this.lastMouseX,
+			this.tiltFx.lineEq(6, 0, this.width, 0, this.mouseX),
+			0.05
+		);
+		const newScrollingPos = window.pageYOffset;
+		this.lastMouseY = this.tiltFx.lerp(
+			this.lastMouseY,
+			this.tiltFx.lineEq(0, 65, this.docheight, 0, newScrollingPos),
+			0.05
+		);
+		this.lastScale = this.tiltFx.lerp(
+			this.lastScale,
+			this.tiltFx.lineEq(0, 158, this.docheight, 0, newScrollingPos),
+			0.05
+		);
+		this.camera.position.set(this.lastMouseX, this.lastMouseY, this.lastScale);
+		this.requestId = requestAnimationFrame(() => this.tilt());
+	}
 
-    window.onbeforeunload = () => {
-      window.cancelAnimationFrame(this.requestId);
-      window.scrollTo(0, 0);
-    };
-  }
+	addFloor() {
+		const floor = { color: "#000000" };
+		const planeGeometry = new THREE.PlaneGeometry(200, 200);
+		const planeMaterial = new THREE.MeshStandardMaterial({
+			color: floor.color,
+			metalness: 0,
+			emissive: "#000000",
+			roughness: 0,
+		});
 
-  tilt() {
-    this.lastMouseX = this.tiltFx.lerp(this.lastMouseX, this.tiltFx.lineEq(6, 0, this.width, 0, this.mouseX), 0.05);
-    const newScrollingPos = window.pageYOffset;
-    this.lastMouseY = this.tiltFx.lerp(this.lastMouseY, this.tiltFx.lineEq(0, 65, this.docheight, 0, newScrollingPos), 0.05);
-    this.lastScale = this.tiltFx.lerp(this.lastScale, this.tiltFx.lineEq(0, 158, this.docheight, 0, newScrollingPos), 0.05);
-    this.camera.position.set(this.lastMouseX, this.lastMouseY, this.lastScale);
-    this.requestId = requestAnimationFrame(() => this.tilt());
-  }
+		const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-  addFloor() {
-    const floor = { color: '#000000' };
-    const planeGeometry = new THREE.PlaneGeometry(200, 200);
-    const planeMaterial = new THREE.MeshStandardMaterial({
-      color: floor.color,
-      metalness: 0,
-      emissive: '#000000',
-      roughness: 0,
-    });
+		planeGeometry.rotateX(-Math.PI / 2);
+		plane.position.y = 0;
 
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+		this.scene.add(plane);
+	}
 
-    planeGeometry.rotateX(- Math.PI / 2);
-    plane.position.y = 0;
+	addPointLight(params) {
+		const pointLight = new THREE.PointLight(params.color, params.intensity);
 
-    this.scene.add(plane);
-  }
+		pointLight.position.set(
+			params.position.x,
+			params.position.y,
+			params.position.z
+		);
 
-  addPointLight(params) {
-    const pointLight = new THREE.PointLight(params.color, params.intensity);
+		this.scene.add(pointLight);
+	}
 
-    pointLight.position.set(params.position.x, params.position.y, params.position.z);
+	getRandomBuiding() {
+		return this.models[
+			Math.floor(Math.random() * Math.floor(this.models.length))
+		];
+	}
 
-    this.scene.add(pointLight);
-  }
+	onLoadModelsComplete(obj) {
+		this.models = [...obj.children].map((model) => {
+			const scale = 0.01;
 
-  getRandomBuiding() {
-    return this.models[Math.floor(Math.random() * Math.floor(this.models.length))];
-  }
+			model.scale.set(scale, scale, scale);
+			model.position.set(0, -14, 0);
+			model.receiveShadow = true;
+			model.castShadow = true;
 
-  onLoadModelsComplete(obj) {
-    this.models = [...obj.children].map((model) => {
-      const scale = .01;
+			return model;
+		});
 
-      model.scale.set(scale, scale, scale);
-      model.position.set(0, -14, 0);
-      model.receiveShadow = true;
-      model.castShadow = true;
+		this.draw();
 
-      return model;
-    });
+		setTimeout(() => {
+			this.removeLoader();
+			this.showBuildings();
+		}, 500);
 
-    this.draw();
+		window.addEventListener("resize", this.onResize.bind(this));
+	}
 
-    setTimeout(() => {
-      this.removeLoader();
-      this.showBuildings();
-    }, 500);
+	removeLoader() {
+		document.querySelector(".loader").classList.add("loader--done");
+	}
 
-    window.addEventListener('resize', this.onResize.bind(this));
-  }
+	showBuildings() {
+		this.sortBuildingsByDistance();
 
-  removeLoader() {
-    document.querySelector('.loader').classList.add('loader--done');
-  }
+		this.buildings.forEach((building, index) => {
+			TweenMax.to(building.position, 0.6 + index / 4000, {
+				y: 1,
+				ease: Quint.easeOut,
+				delay: index / 4000,
+			});
+		});
+	}
 
-  showBuildings() {
-    this.sortBuildingsByDistance();
+	sortBuildingsByDistance() {
+		this.buildings
+			.sort((a, b) => {
+				if (a.position.z > b.position.z) {
+					return 1;
+				}
+				if (a.position.z < b.position.z) {
+					return -1;
+				}
+				return 0;
+			})
+			.reverse();
+	}
 
-    this.buildings.forEach((building, index) => {
-      TweenMax.to(building.position, .6 + (index / 4000), { y: 1, ease: Quint.easeOut, delay: index / 4000 });
-    });
-  }
+	loadModels(name, callback) {
+		const objLoader = new THREE.OBJLoader();
 
-  sortBuildingsByDistance() {
-    this.buildings.sort((a, b) => {
-      if (a.position.z > b.position.z) {
-        return 1;
-      }
-      if (a.position.z < b.position.z) {
-        return -1;
-      }
-      return 0;
-    }).reverse();
-  }
+		objLoader.load(name, callback);
+	}
 
-  loadModels(name, callback) {
-    const objLoader = new THREE.OBJLoader();
+	draw() {
+		const boxSize = 3;
+		const meshParams = {
+			color: "#000",
+			metalness: 0,
+			emissive: "#000",
+			roughness: 0.77,
+		};
 
-    objLoader.load(name, callback);
-  }
+		const max = 0.009;
+		const min = 0.001;
+		const material = new THREE.MeshPhysicalMaterial(meshParams);
 
-  draw() {
-    const boxSize = 3;
-    const meshParams = {
-      color: '#000',
-      metalness: 0,
-      emissive: '#000',
-      roughness: .77,
-    };
+		for (let i = 0; i < this.gridSize; i++) {
+			for (let j = 0; j < this.gridSize; j++) {
+				const building = this.getRandomBuiding().clone();
 
-    const max = .009;
-    const min = .001;
-    const material = new THREE.MeshPhysicalMaterial(meshParams);
+				building.material = material;
+				building.scale.y = Math.random() * (max - min + 0.01);
+				building.position.x = i * boxSize;
+				building.position.z = j * boxSize;
 
-    for (let i = 0; i < this.gridSize; i++) {
-      for (let j = 0; j < this.gridSize; j++) {
-        const building = this.getRandomBuiding().clone();
+				this.group.add(building);
 
-        building.material = material;
-        building.scale.y = Math.random() * (max - min + .01);
-        building.position.x = (i * boxSize);
-        building.position.z = (j * boxSize);
+				this.buildings.push(building);
+			}
+		}
 
-        this.group.add(building);
+		this.scene.add(this.group);
+		this.group.position.set(-this.gridSize - 10, 1, -this.gridSize - 10);
+	}
 
-        this.buildings.push(building);
-      }
-    }
+	onResize() {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
 
-    this.scene.add(this.group);
-    this.group.position.set(-this.gridSize - 10, 1, -this.gridSize - 10);
-  }
+		this.camera.aspect = this.width / this.height;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(this.width, this.height);
+	}
 
-  onResize() {
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
+	animate() {
+		this.controls.update();
 
-    this.camera.aspect = this.width / this.height;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(this.width, this.height);
-  }
+		this.renderer.render(this.scene, this.camera);
 
-  animate() {
-    this.controls.update();
-
-    this.renderer.render(this.scene, this.camera);
-
-    requestAnimationFrame(this.animate.bind(this));
-  }
+		requestAnimationFrame(this.animate.bind(this));
+	}
 }
