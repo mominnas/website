7e757028199558e0cfc3bb0e9b4deb967e3965c0
Momin Naseem <mominn3935@gmail.com>
2023-03-06 00:11:49 -0500

    Projects page styles

--- src/scripts/projects.ts
+++ src/scripts/projects.ts
@@ -5,40 +5,41 @@ export default class App {
     toggled: boolean;
     columns: number;
     rows: number;
-    tiles: HTMLElement | null;
+    gridCells: HTMLElement | null;
     
     constructor() {
     
         this.columns = 0
         this.rows = 0
         this.toggled = false;
-        this.tiles = null;
+        this.gridCells = null;
     
     }
     
     
     init() {
     
-        this.tiles = document.getElementById("tiles");
+        this.gridCells = document.getElementById("cells");
         this.generateGrid();
         window.addEventListener('resize', this.generateGrid);
     }
 
-
-    
-
     
 
     toggleHandler = (): void => {
+
         this.toggled = !this.toggled;
-        document.body.classList.toggle("toggled", true);
+        // document.body.classList.toggle("toggled");
+        document.getElementById("gridcard")!.classList.toggle("toggled");
+    
     }
 
     clickHandler = (index: number) => {
+        
         this.toggleHandler();
     
         anime({
-            targets: ".tile",
+            targets: ".cell",
             opacity: this.toggled ? 0 : 1,
             delay: anime.stagger(50, {
             grid: [this.columns, this.rows],
@@ -48,42 +49,45 @@ export default class App {
     
     }
 
-    createTile = (index: number) => {
+    createCell = (index: number) => {
         
         // create a new div element
-        const tile: HTMLDivElement = document.createElement("div");
+        const cell: HTMLDivElement = document.createElement("div");
 
-        // add the class "tile" to it
-        tile.classList.add("tile");
+        // add the class "cell" to it
+        cell.classList.add("cell");
         
         // set the opacity to 0 if it's toggled, or 1 if it isn't
-        tile.style.opacity = String(this.toggled ? 0 : 1);
+        cell.style.opacity = String(this.toggled ? 0 : 1);
         
         // set the onclick method to the clickHandler, passing the index
-        tile.onclick = e => this.clickHandler(index);
+        cell.onclick = e => this.clickHandler(index);
         
-        return tile;
+        return cell;
     }
 
-    generateTiles = (quantity: number) => {
-    Array.from(Array(quantity)).map((tile, index) => {
-        this.tiles!.appendChild(this.createTile(index));
+    
+    generateCells = (quantity: number) => {
+    
+        Array.from(Array(quantity)).map((tile, index) => {
+            this.gridCells!.appendChild(this.createCell(index));
         });
+    
     }
 
     generateGrid = () => {
         // Set the grid to the size of the window
-        this.tiles!.innerHTML = "";
+        this.gridCells!.innerHTML = "";
         const size = document.body.clientWidth > 800 ? 100 : 50;
         this.columns = Math.floor(document.body.clientWidth / size);
         this.rows = Math.floor(document.body.clientHeight / size);
         
         // Set the CSS grid properties
-        this.tiles!.style.setProperty("--columns", String(this.columns));
-        this.tiles!.style.setProperty("--rows", String(this.rows));
+        this.gridCells!.style.setProperty("--columns", String(this.columns));
+        this.gridCells!.style.setProperty("--rows", String(this.rows));
         
         // Generate the tiles
-        this.generateTiles(this.columns * this.rows);
+        this.generateCells(this.columns * this.rows);
     }
 
 
